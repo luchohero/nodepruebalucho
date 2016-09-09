02cfac88f098315fbb6582ca71a3e3075d1dc311
Luis Cardenas <info@luchohero.com>
2016-09-09 00:18:19 -0500

    index.html carpeta
    :D

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + 'public/'));
-var port = process.env.PORT || 7000;
-app.listen(port);
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
